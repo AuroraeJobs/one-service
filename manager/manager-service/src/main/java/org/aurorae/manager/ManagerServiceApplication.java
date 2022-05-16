43ad908c275f69882376551d406d1fc0d92ae734
Aurorae <aurorae_jobs@hotmail.com>
2022-05-16 12:41:13 +0800

    提供公共的service-pom模块

--- manager/manager-service/src/main/java/org/aurorae/manager/ManagerServiceApplication.java
+++ manager/manager-service/src/main/java/org/aurorae/manager/ManagerServiceApplication.java
@@ -2,11 +2,16 @@ package org.aurorae.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.AdviceMode;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author aurorae
  */
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching(mode = AdviceMode.ASPECTJ)
 public class ManagerServiceApplication {
 
     public static void main(String[] args) {
